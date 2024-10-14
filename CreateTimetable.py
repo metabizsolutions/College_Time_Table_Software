78ec21afba2d65209c32bc448a425e95a2ef83bb
asad-irfan-github <asadirfanmzg@gmail.com>
2024-10-15 01:11:33 +0500

    17

--- CreateTimetable.py
+++ CreateTimetable.py
@@ -1,6 +1,16 @@
 import sys
 import sqlite3
-from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QLabel, QComboBox, QLineEdit, QPushButton, QMessageBox
+from PyQt5.QtWidgets import (
+    QApplication,
+    QWidget,
+    QVBoxLayout,
+    QLabel,
+    QLineEdit,
+    QPushButton,
+    QMessageBox,
+    QListWidget,
+    QComboBox
+)
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QFont
 
@@ -26,59 +36,83 @@ class CreateTimetableWindow(QWidget):
         self.setLayout(self.layout)
 
     def create_input_fields(self):
-        # Department selection
-        self.department_label = QLabel("Select Department")
+        # Department input
+        self.department_label = QLabel("Search Department")
         self.department_label.setFont(QFont("Georgia", 14))
         self.layout.addWidget(self.department_label)
 
-        self.department_combo = QComboBox()
-        self.fetch_data("Programs", "program_name", self.department_combo)
-        self.layout.addWidget(self.department_combo)
+        self.department_input = QLineEdit(self)
+        self.department_input.textChanged.connect(lambda: self.auto_complete("Programs", "program_name", self.department_input, self.department_list))
+        self.layout.addWidget(self.department_input)
 
-        # Semester selection
-        self.semester_label = QLabel("Select Semester")
+        self.department_list = QListWidget(self)
+        self.department_list.itemClicked.connect(self.select_department)
+        self.layout.addWidget(self.department_list)
+
+        # Semester input
+        self.semester_label = QLabel("Search Semester")
         self.semester_label.setFont(QFont("Georgia", 14))
         self.layout.addWidget(self.semester_label)
 
-        self.semester_combo = QComboBox()
-        self.fetch_data("Programs", "semester", self.semester_combo)
-        self.layout.addWidget(self.semester_combo)
+        self.semester_input = QLineEdit(self)
+        self.semester_input.textChanged.connect(lambda: self.auto_complete("Programs", "semester", self.semester_input, self.semester_list))
+        self.layout.addWidget(self.semester_input)
 
-        # Teacher selection
-        self.teacher_label = QLabel("Select Teacher")
+        self.semester_list = QListWidget(self)
+        self.semester_list.itemClicked.connect(self.select_semester)
+        self.layout.addWidget(self.semester_list)
+
+        # Teacher input
+        self.teacher_label = QLabel("Search Teacher")
         self.teacher_label.setFont(QFont("Georgia", 14))
         self.layout.addWidget(self.teacher_label)
 
-        self.teacher_combo = QComboBox()
-        self.fetch_data("Teachers", "teacher_name", self.teacher_combo)
-        self.layout.addWidget(self.teacher_combo)
+        self.teacher_input = QLineEdit(self)
+        self.teacher_input.textChanged.connect(lambda: self.auto_complete("Teachers", "teacher_name", self.teacher_input, self.teacher_list))
+        self.layout.addWidget(self.teacher_input)
 
-        # Course Title selection
-        self.course_title_label = QLabel("Select Course Title")
+        self.teacher_list = QListWidget(self)
+        self.teacher_list.itemClicked.connect(self.select_teacher)
+        self.layout.addWidget(self.teacher_list)
+
+        # Course Title input
+        self.course_title_label = QLabel("Search Course Title")
         self.course_title_label.setFont(QFont("Georgia", 14))
         self.layout.addWidget(self.course_title_label)
 
-        self.course_title_combo = QComboBox()
-        self.fetch_data("Courses", "course_name", self.course_title_combo)
-        self.layout.addWidget(self.course_title_combo)
+        self.course_title_input = QLineEdit(self)
+        self.course_title_input.textChanged.connect(lambda: self.auto_complete("Courses", "course_name", self.course_title_input, self.course_title_list))
+        self.layout.addWidget(self.course_title_input)
 
-        # Course Code selection
-        self.course_code_label = QLabel("Select Course Code")
+        self.course_title_list = QListWidget(self)
+        self.course_title_list.itemClicked.connect(self.select_course_title)
+        self.layout.addWidget(self.course_title_list)
+
+        # Course Code input
+        self.course_code_label = QLabel("Search Course Code")
         self.course_code_label.setFont(QFont("Georgia", 14))
         self.layout.addWidget(self.course_code_label)
 
-        self.course_code_combo = QComboBox()
-        self.fetch_data("Courses", "course_code", self.course_code_combo)
-        self.layout.addWidget(self.course_code_combo)
+        self.course_code_input = QLineEdit(self)
+        self.course_code_input.textChanged.connect(lambda: self.auto_complete("Courses", "course_code", self.course_code_input, self.course_code_list))
+        self.layout.addWidget(self.course_code_input)
 
-        # Classroom selection
-        self.classroom_label = QLabel("Select Classroom")
+        self.course_code_list = QListWidget(self)
+        self.course_code_list.itemClicked.connect(self.select_course_code)
+        self.layout.addWidget(self.course_code_list)
+
+        # Classroom input
+        self.classroom_label = QLabel("Search Classroom")
         self.classroom_label.setFont(QFont("Georgia", 14))
         self.layout.addWidget(self.classroom_label)
 
-        self.classroom_combo = QComboBox()
-        self.fetch_data("Classrooms", "classroom_name", self.classroom_combo)
-        self.layout.addWidget(self.classroom_combo)
+        self.classroom_input = QLineEdit(self)
+        self.classroom_input.textChanged.connect(lambda: self.auto_complete("Classrooms", "classroom_name", self.classroom_input, self.classroom_list))
+        self.layout.addWidget(self.classroom_input)
+
+        self.classroom_list = QListWidget(self)
+        self.classroom_list.itemClicked.connect(self.select_classroom)
+        self.layout.addWidget(self.classroom_list)
 
         # Time input
         self.time_label = QLabel("Enter Time")
@@ -97,22 +131,55 @@ class CreateTimetableWindow(QWidget):
         self.session_combo.addItems(["Morning", "Evening"])
         self.layout.addWidget(self.session_combo)
 
-    def fetch_data(self, table, column, combo_box):
-        """Fetch data from the database and populate the combo box."""
-        query = f"SELECT DISTINCT {column} FROM {table}"
-        self.cursor.execute(query)
+    def auto_complete(self, table, column, input_field, list_widget):
+        """Perform a real-time search in the database and populate the list widget."""
+        search_text = input_field.text()
+        query = f"SELECT DISTINCT {column} FROM {table} WHERE {column} LIKE ?"
+        self.cursor.execute(query, (f"%{search_text}%",))
         rows = self.cursor.fetchall()
+
+        list_widget.clear()  # Clear the previous results
         for row in rows:
-            combo_box.addItem(row[0])
+            list_widget.addItem(row[0])  # Add items to the list widget
+
+    def select_department(self, item):
+        """Set the selected department in the input field."""
+        self.department_input.setText(item.text())
+        self.department_list.clear()  # Clear the list after selection
+
+    def select_semester(self, item):
+        """Set the selected semester in the input field."""
+        self.semester_input.setText(item.text())
+        self.semester_list.clear()  # Clear the list after selection
+
+    def select_teacher(self, item):
+        """Set the selected teacher in the input field."""
+        self.teacher_input.setText(item.text())
+        self.teacher_list.clear()  # Clear the list after selection
+
+    def select_course_title(self, item):
+        """Set the selected course title in the input field."""
+        self.course_title_input.setText(item.text())
+        self.course_title_list.clear()  # Clear the list after selection
+
+    def select_course_code(self, item):
+        """Set the selected course code in the input field."""
+        self.course_code_input.setText(item.text())
+        self.course_code_list.clear()  # Clear the list after selection
+
+    def select_classroom(self, item):
+        """Set the selected classroom in the input field."""
+        self.classroom_input.setText(item.text())
+        self.classroom_list.clear()  # Clear the list after selection
 
     def submit_data(self):
         """Submit the selected data to the database."""
-        department = self.department_combo.currentText()
-        semester = self.semester_combo.currentText()
-        teacher = self.teacher_combo.currentText()
-        course_title = self.course_title_combo.currentText()
-        course_code = self.course_code_combo.currentText()
-        classroom = self.classroom_combo.currentText()
+        department = self.department_input.text()
+        semester = self.semester_input.text()
+        teacher = self.teacher_input.text()
+        course_title = self.course_title_input.text()
+        course_code = self.course_code_input.text()
+        classroom = self.classroom_input.text()
         time = self.time_input.text()
         session = self.session_combo.currentText()
 
